1d6ed1652dae17554b798937338fe2e3e002d604
Zumo <apvictor00@hotmail.com>
2016-08-09 15:10:21 +0200

    Update config.py

--- src/pyshark/config.py
+++ src/pyshark/config.py
@@ -5,4 +5,7 @@ import os
 CONFIG_PATH = os.path.join(os.path.dirname(pyshark.__file__), 'config.ini')
 
 def get_config():
-    return py.iniconfig.IniConfig(CONFIG_PATH)
+	if os.path.isfile(CONFIG_PATH):
+		return py.iniconfig.IniConfig(CONFIG_PATH)
+	else:
+		return None
